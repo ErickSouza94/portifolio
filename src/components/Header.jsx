ee0de5df2be29544c8e2bfc1b60f6d78588e5690
ErickSouza94 <erick.ssouza94@gmail.com>
2025-03-08 00:13:35 -0300

    fetch: add mobile menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,74 @@
+import { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <div className="flex justify-between items-center p-8 border-b-1 border-gray-500 w-full bg-black ">
-        <h2 className="text-4xl font-semibold"><a href="/">Erick Souza</a></h2>
-    
-        <ul className="flex gap-5">
-          <a href="">
-            <li>Projetos</li>
-          </a>
-          <a href="">
-            <li>Sobre</li>
-          </a>
-          <a href="">
-            <li>Contato</li>
-          </a>
-        </ul>
-      
-    </div>
+    <header className="relative flex justify-between items-center p-6 border-b border-gray-500 w-full bg-black">
+      {/* Logo */}
+      <h2 className="text-4xl font-semibold">
+        <a href="/">Erick Souza</a>
+      </h2>
+
+      {/* Ícone do Menu (Fica sempre visível) */}
+      <div
+        className="md:hidden text-3xl cursor-pointer z-50"
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        {menuOpen ? <FaTimes /> : <FaBars />}
+      </div>
+
+      {/* Menu Desktop */}
+      <ul className="hidden md:flex gap-5">
+        <li>
+          <a href="#">Projetos</a>
+        </li>
+        <li>
+          <a href="#">Sobre</a>
+        </li>
+        <li>
+          <a href="#">Contato</a>
+        </li>
+      </ul>
+
+      {menuOpen && (
+        <div
+          className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm transition-opacity duration-300"
+          onClick={() => setMenuOpen(false)}
+        />
+      )}
+
+      {/* Menu Mobile */}
+      <div
+        className={`fixed top-0 left-0 w-full py-30 bg-black bg-opacity-95 flex flex-col items-center justify-center text-2xl bg-opacity-0 border-2 border-gray-500 transition-transform duration-300 ${
+          menuOpen ? "translate-y-0" : "-translate-y-full "
+        }`}
+      >
+        {/* Links do Menu */}
+        <a
+          href="#"
+          className="mb-4 border-2 w-full p-5 text-center"
+          onClick={() => setMenuOpen(false)}
+        >
+          Projetos
+        </a>
+        <a
+          href="#"
+          className="mb-4 border-2 w-full p-5 text-center"
+          onClick={() => setMenuOpen(false)}
+        >
+          Sobre
+        </a>
+        <a
+          href="#"
+          className="mb-4 border-2 w-full p-5 text-center"
+          onClick={() => setMenuOpen(false)}
+        >
+          Contato
+        </a>
+      </div>
+    </header>
   );
 };
 
