3f2427469c5b64d0f7d34ffb01201139be981fb7
ErickSouza94 <erick.ssouza94@gmail.com>
2025-03-13 23:26:11 -0300

    refactor: add conditional class for centering in Tools component

--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -1,3 +1,5 @@
+import { useLocation } from "react-router-dom"
+
 import html from "/html-logo.svg"
 import css from "/css.svg"
 import js from "/js.svg"
@@ -11,8 +13,12 @@ import mongo from "/mongo.svg"
 import express from "/express.svg"
 
 const Tools = () => {
+  const location = useLocation()
+  const isMoreAbout = location.pathname === '/more-about'
+  
+
   return (
-    <div className="p-5 flex items-center gap-5 flex-wrap max-w-[800px]">
+    <div className={`p-5 flex items-center gap-5 flex-wrap max-w-[800px] ${isMoreAbout && 'justify-center'}`}>
             <img src={html} alt="" className="md:w-[16%] h-[10%] max-w-[24%] sm:w-[20%] hover:scale-110 rounded-t-xl"/>
             <img src={css} alt="" className="md:w-[15%] h-[10%] max-w-[24%] sm:w-[20%] hover:scale-110 rounded-t-xl"/>
             <img src={js} alt="" className="md:w-[20%] h-[10%] max-w-[24%] sm:w-[20%] hover:scale-110 rounded-t-xl"/>
