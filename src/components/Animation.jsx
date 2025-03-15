446064365d02cd3c963b5780eaaa9021ca78d8c4
ErickSouza94 <erick.ssouza94@gmail.com>
2025-03-15 16:16:55 -0300

    fix: correct spelling of "PORTFÓLIO" in Animation component

--- src/components/Animation.jsx
+++ src/components/Animation.jsx
@@ -6,7 +6,7 @@ const Animation = () => {
       sequence={[
         "OLÁ, EU ME CHAMO \nERICK SOUZA",
         2000,
-        "SEJA BEM-VINDO(A) AO MEU PORTIFÓLIO!",
+        "SEJA BEM-VINDO(A) AO MEU PORTFÓLIO!",
         2000,
       ]}
       wrapper="h1"
